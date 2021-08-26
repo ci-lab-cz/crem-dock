5e928f53739e5230ed4c6c7876c93a8193bab748
Aleksandra <avnikonenko.volg@gmail.com>
2021-08-26 17:09:27 +0200

    Use vina2 python version

--- grow.py
+++ grow.py
@@ -9,7 +9,7 @@ import string
 import subprocess
 import sys
 import traceback
-from itertools import combinations
+from itertools import combinations, chain
 from multiprocessing import Pool, cpu_count
 
 import matplotlib.pyplot as plt
@@ -26,7 +26,7 @@ from rdkit.Chem.rdMolDescriptors import CalcNumRotatableBonds
 from rdkit.ML.Cluster import Butina
 from scipy.spatial import distance_matrix
 
-from scripts import Docking, Smi2PDB
+from scripts import Docking
 
 
 def cpu_type(x):
@@ -146,7 +146,8 @@ def get_score(pdb_block):
     return score_correct
 
 
-def update_db(conn, dname, protonation):
+
+def update_db(conn, dock_result, mol_ids, protonation):
     """
     Insert score, rmsd, fixed bonds string, pdb and mol blocks of molecules having a corresponding pdbqt file in the
     temporary dir in docking DB
@@ -154,55 +155,46 @@ def update_db(conn, dname, protonation):
     :param dname: path to temp directory with files after docking. Those files should have names <mol_id>_dock.pdbqt
     :return:
     """
+    cur = conn.cursor()
+    parent_ids = list(chain.from_iterable(cur.execute(f'SELECT DISTINCT parent_id FROM mols WHERE id IN ({",".join("?" * len(mol_ids))})', mol_ids)))
+    parents_dict = dict(cur.execute(f'SELECT id, mol_block FROM mols WHERE id IN ({",".join("?" * len(parent_ids))})', parent_ids))
 
-    for fname in glob.glob(os.path.join(dname, '*_dock.pdbqt')):
-        cur = conn.cursor()
-
-        # get pdb block
-        pdb_block = open(fname).read()
-
-        # get score
-        score = get_score(pdb_block)
-
-        # get mol block for the first pose
-        mol_id = os.path.basename(fname).replace('_dock.pdbqt', '')
+    for mol_id, [score, pose_block] in zip(mol_ids, dock_result):
         if protonation:
             smi = list(cur.execute(f"SELECT smi_protonated FROM mols WHERE id = '{mol_id}'"))[0][0]
         else:
             smi = list(cur.execute(f"SELECT smi FROM mols WHERE id = '{mol_id}'"))[0][0]
-
         mol_block = None
-        mol = Chem.MolFromPDBBlock('\n'.join([i[:66] for i in pdb_block.split('MODEL')[1].split('\n')]),
+        mol = Chem.MolFromPDBBlock('\n'.join([i[:66] for i in pose_block.split('MODEL')[1].split('\n')]),
                                    removeHs=False)
-
+        parent_id = list(cur.execute(f"SELECT parent_id FROM mols WHERE id = '{mol_id}'"))[0][0]
+        parent_mol_block = parents_dict[parent_id]
         if mol:
             try:
                 mol = AllChem.AssignBondOrdersFromTemplate(Chem.AddHs(Chem.MolFromSmiles(smi), explicitOnly=True), mol)
                 mol.SetProp('_Name', mol_id)
                 mol_block = Chem.MolToMolBlock(mol)
             except:
-                sys.stderr.write(f'Could not assign bond orders while parsing PDB: {fname}\n')
+                sys.stderr.write(f'Could not assign bond orders while parsing pose: {mol_id}\n')
                 mol = None
-            # get atoms
-            parent_id = list(cur.execute(f"SELECT parent_id FROM mols WHERE id = '{mol_id}'"))[0][0]
-            if parent_id and mol:
-                parent_mol_block = list(cur.execute(f"SELECT mol_block FROM mols WHERE id = '{parent_id}'"))[0][0]
+            if parent_mol_block and mol:
                 parent_mol = Chem.MolFromMolBlock(parent_mol_block)
                 rms = get_rmsd(mol, parent_mol)
             else:
                 rms = None
         else:
-            sys.stderr.write(f'Could not read PDB: {fname}\n')
+            sys.stderr.write(f'Could not read pose: {mol_id}\n')
             rms = None
 
         cur.execute("""UPDATE mols
-                           SET pdb_block = ?,
-                               mol_block = ?,
-                               docking_score = ?,
-                               rmsd = ? 
-                           WHERE
-                               id = ?
-                        """, (pdb_block.split('MODEL 2')[0], mol_block, score, rms, mol_id))
+                                   SET pdb_block = ?,
+                                       mol_block = ?,
+                                       docking_score = ?,
+                                       rmsd = ? 
+                                   WHERE
+                                       id = ?
+                                """, (pose_block, mol_block, score, rms, mol_id))
+
     conn.commit()
 
 
@@ -247,6 +239,9 @@ def get_mols(conn, mol_ids):
     sql = f'SELECT mol_block, protected_user_canon_ids FROM mols WHERE id IN ({",".join("?" * len(mol_ids))})'
     mols = []
     for items in cur.execute(sql, mol_ids):
+        # remove it
+        if items[0] is None:
+            continue
         m = Chem.MolFromMolBlock(items[0], removeHs=False)
         if not m:
             continue
@@ -743,19 +738,14 @@ def get_canon_for_atom_idx(mol, idx):
 
 
 def make_iteration(conn, iteration, protein_pdbqt, protein_setup, ntop, tanimoto, mw, rmsd, rtb, alg_type,
-                   ncpu, tmpdir, vina_path, python_path, vina_script_dir, protonation, debug,
-                   make_docking=True, make_selection=True, **kwargs):
+                   ncpu, tmpdir, protonation, debug, make_docking=True, make_selection=True, **kwargs):
     if make_docking:
-        tmpdir = os.path.abspath(os.path.join(tmpdir, f'iter{iteration}'))
-        os.makedirs(tmpdir)
-        save_smi_to_pdb(conn, iteration, tmpdir, protonation, ncpu)
-        prep_ligands(conn, tmpdir, python_path, vina_script_dir, ncpu)
-        dock_ligands(tmpdir, protein_pdbqt, protein_setup, vina_path, ncpu)
-        update_db(conn, tmpdir, protonation)
-        if not debug:
-            shutil.rmtree(tmpdir, ignore_errors=True)
+        dock_result, mol_ids = Docking.iter_docking(conn, receptor_pdbqt_fname=protein_pdbqt, protein_setup=protein_setup,
+                             protonation=protonation, iteration=iteration, ncpu=ncpu)
+        update_db(conn, dock_result, mol_ids, protonation)
+    else:
+        mol_ids = get_docked_mol_ids(conn, iteration)
 
-    mol_ids = get_docked_mol_ids(conn, iteration)
     mols = get_mols(conn, mol_ids)
 
     res = []
@@ -853,10 +843,6 @@ def main():
     parser.add_argument('--no_protonation', action='store_true', default=False,
                         help='disable protonation of molecules before docking. Protonation requires installed '
                              'cxcalc chemaxon utility.')
-    parser.add_argument('--mgl_install_dir', metavar='DIRNAME', required=True,
-                        help='path to the dir with installed MGLtools.')
-    parser.add_argument('--vina', metavar='vina_path', required=True,
-                        help='path to the vina executable.')
     parser.add_argument('-t', '--algorithm', default=1, type=int,
                         help='the number of the search algorithm: 1 - greedy search, 2 - deep clustering, '
                              '3 - clustering, 4 - Pareto front.')
@@ -877,9 +863,6 @@ def main():
                         help='number of cpus. Default: 1.')
 
     args = parser.parse_args()
-
-    python_path = os.path.join(args.mgl_install_dir, 'bin/python')
-    vina_script_dir = os.path.join(args.mgl_install_dir, 'MGLToolsPckgs/AutoDockTools/Utilities24/')
 
     if args.tmpdir is None:
         tmpdir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(args.output)),
@@ -917,8 +900,7 @@ def main():
             res = make_iteration(conn=conn, iteration=iteration, protein_pdbqt=args.protein,
                                  protein_setup=args.protein_setup, ntop=args.ntop, tanimoto=index_tanimoto,
                                  mw=args.mol_weight, rmsd=args.rmsd, rtb=args.rotatable_bonds, alg_type=args.algorithm,
-                                 ncpu=args.ncpu, tmpdir=tmpdir, vina_path=args.vina, python_path=python_path,
-                                 vina_script_dir=vina_script_dir, make_docking=make_docking,
+                                 ncpu=args.ncpu, tmpdir=tmpdir, make_docking=make_docking,
                                  make_selection=make_selection,
                                  db_name=args.db, radius=args.radius, min_freq=args.min_freq,
                                  min_atoms=args.min_atoms, max_atoms=args.max_atoms,
