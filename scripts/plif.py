86d0185e0c9da020b53c33b03fd2f6b3acd04d1f
guzelka <minibaeva07501@gmail.com>
2022-08-12 15:10:18 +0200

    add definition of interactions in line 47

--- scripts/plif.py
+++ scripts/plif.py
@@ -44,7 +44,8 @@ def filter_by_plif(mols, plif_ref, protein_fname, threshold=1):
     if len(mols) == 0:
         return []
     prot = plf.Molecule(Chem.MolFromPDBFile(protein_fname, removeHs=False, sanitize=False))
-    fp = plf.Fingerprint()
+    fp = plf.Fingerprint(['Hydrophobic', 'HBDonor', 'HBAcceptor', 'Anionic', 'Cationic', 'CationPi', 'PiCation',
+                          'FaceToFace', 'EdgeToFace', 'MetalAcceptor'])
     fp.run_from_iterable((plf.Molecule.from_rdkit(mol) for mol in mols), prot)   # danger, hope it will always keep the order of molecules
     df = fp.to_dataframe()
     df.columns = [''.join(item.strip().lower() for item in items[1:]) for items in df.columns]
