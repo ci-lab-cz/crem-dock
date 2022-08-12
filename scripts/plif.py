5ad46b737a8b4fd0b50f6dd222c23e17009a240c
guzelka <minibaeva07501@gmail.com>
2022-08-12 15:03:01 +0200

    add definition of interactions

--- scripts/plif.py
+++ scripts/plif.py
@@ -69,7 +69,8 @@ def plif_similarity(mol, plif_protein_fname, plif_ref_df):
     :return:
     """
     plf_prot = plf.Molecule(Chem.MolFromPDBFile(plif_protein_fname, removeHs=False, sanitize=False))
-    fp = plf.Fingerprint()
+    fp = plf.Fingerprint(['Hydrophobic', 'HBDonor', 'HBAcceptor', 'Anionic', 'Cationic', 'CationPi', 'PiCation',
+                          'FaceToFace', 'EdgeToFace', 'MetalAcceptor'])
     try:
         fp.run_from_iterable([plf.Molecule.from_rdkit(mol)], plf_prot)   # danger, hope it will always keep the order of molecules
     except AssertionError:  # catch multiprocessing conflict with new version of prolif
