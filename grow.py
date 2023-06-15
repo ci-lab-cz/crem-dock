7c6bac3a1e84522a695b46ca1b50023b82a2b0a2
guzelka <minibaeva07501@gmail.com>
2023-06-15 15:47:01 +0200

    del tautomer_refinement function

--- grow.py
+++ grow.py
@@ -899,53 +899,6 @@ def ranking_by_num_heavy_atoms_fcsp3_bm(conn, mol_ids):
     return stat_scores
 
 
-# def tautomer_refinement(conn, ncpu):
-#     cur = conn.cursor()
-#     smiles_dict = dict(cur.execute("SELECT smi, id FROM mols WHERE iteration != 0"))
-#
-#     if not smiles_dict:
-#         sys.stderr.write('There are no molecules in database after growing\n')
-#         return False
-#
-#     smiles, mol_ids = zip(*iter(smiles_dict.items()))
-#
-#     with tempfile.NamedTemporaryFile(suffix='.smi', mode='w', encoding='utf-8') as tmp:
-#         fd, output = tempfile.mkstemp()
-#         try:
-#             tmp.writelines(['\n'.join(smiles)])
-#             tmp.flush()
-#             cmd_run = f"cxcalc -S majortautomer -f smiles -a false '{tmp.name}' > '{output}'"
-#             subprocess.call(cmd_run, shell=True)
-#             tautomers = Chem.SDMolSupplier(output)
-#         finally:
-#             os.remove(output)
-#
-#     data = list()
-#     for smi, tautomer, id_ in zip(smiles, tautomers, mol_ids):
-#         stable_tautomer = tautomer.GetPropsAsDict().get('MAJOR_TAUTOMER', None)
-#         if stable_tautomer is not None:
-#             tautomer_smi = Chem.CanonSmiles(stable_tautomer)
-#             if tautomer_smi != smi:
-#                 data.append((id_, tautomer_smi))
-#
-#     if data:
-#         cols = ['id', 'smi']
-#         insert_db(conn, data, cols, table_name='tautomers')
-#
-#         cur.execute("""UPDATE tautomers
-#                            SET
-#                               docking_score = mols.docking_score,
-#                               duplicate = mols.id
-#                            FROM
-#                               mols
-#                            WHERE
-#                                mols.smi = tautomers.smi
-#                         """)
-#         conn.commit()
-#         return True
-#     else:
-#         return False
-
 def get_major_tautomer(mol_dict):
     """
     convert child molecules with parent mol names to smiles file, tautomerize and return the same data structure as input
